e202444fd24b11001509754d5d6e45ed33a83e52
Parth Bhardwaj <pbhardwaj@datopic.com>
2024-09-08 19:46:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+
+
+enum GenderEnum {
+  female = "female",
+  male = "male",
+  other = "other",
+}
+
+
+interface IFormInput {
+  firstName: string
+  gender: GenderEnum
+}
+
+
+export default function App() {
+  const { register, handleSubmit , formState: { errors } } = useForm<IFormInput>()
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+  console.log(GenderEnum.female)
+
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First Name</label>
+      <input {...register("firstName",{required:true})} />
+      {errors.firstName && <span>this is required</span>} 
+      <label>Gender Selection</label>
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <input type="submit" />
+    </form>
+  )
+}
